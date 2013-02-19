bc03b60ff5b2b3d36956763402ff2577cad17d96
espengjerde <espen.gjerde@gmail.com>
2013-02-19 11:25:00 +0000

    added mac-search

--- df_findip.py
+++ df_findip.py
@@ -8,10 +8,9 @@ class DHCP:
 	# We should probably make a configuration-file... 
 	leasefile = '/var/lib/dhcp/dhcpd.leases'
 	ip_filter = r'([\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3})'
-	#ip_filter = r'^lease\ [\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\ \{$'
-	#regex_mac = r'([a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9])' 
+	mac_filter = r'([a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9])' 
 
-	def get_leases(self):
+	def get_ips(self):
 		""" 
 		parses lease-file in \"leasefile\" and returns list of registered ip-adresses
 		"""
@@ -20,6 +19,18 @@ class DHCP:
 		regex_ip = re.compile(self.ip_filter)
 		return regex_ip.findall(text)
 
+	def get_macs(self):
+		file = open(self.leasefile)
+		text = file.read()
+		regex_mac = re.compile(self.mac_filter)
+		return regex_mac.findall(text)
+
+
+	def ip_exists(self, ip_address):
+		return ip_address in self.get_ips()
+
+	def get_leases(self):
+		return dict(zip(self.get_macs(),self.get_ips()))
 
 	def print_leases(self):
 		"""	
@@ -28,5 +39,5 @@ class DHCP:
 		leases = self.get_leases()
 		print "Number of leases: ",len(leases)
 		for lease in leases:
-			print "IP: "+lease
+			print lease.key, ": ",lease.value
 
