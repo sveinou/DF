f7a031b438e53c938e94f8b51c113df68bf73a83
espengjerde <espen.gjerde@gmail.com>
2013-02-26 11:34:13 +0000

    laga enkel loginside + litt trøbilskyting

--- df_login.py
+++ df_login.py
@@ -1,4 +1,4 @@
-#/usr/bin/python
+
 
 import sys
 import subprocess
@@ -14,36 +14,36 @@ from df_firewall import Firewall
 pwd_min_length = 4
 
 def get_input():
-	"""
+    """
 	Gets username,password and IP-address from commandline
 	and pases it on as dictionary.
-	"""
-	if len(argv) != 4:
-		raise ValueError("Not enough/Too many arguments")
+    """
+    if len(sys.argv) != 4:
+        raise ValueError("Not enough/Too many arguments")
 
-	user = argv[1]
-	password = argv[2]
-	ip = argv[3]
-	if type(user) != str:	
-		raise ValueError("User not string!")
-	elif type(password) != str or len(password) < pwd_min_length:
-		raise ValueError("Input error on password")
-	elif type(ip) != str or len(ip) < 7:
-		raise ValueError("error on IP input")
+    user = sys.argv[1]
+    password = sys.argv[2]
+    ip = sys.argv[3]
+    if type(user) != str:	
+        raise ValueError("User not string!")
+    elif type(password) != str or len(password) < pwd_min_length:
+        raise ValueError("Input error on password")
+    elif type(ip) != str or len(ip) < 7:
+        raise ValueError("error on IP input")
 
-	return {'username':user, 'password': password, 'ip_addr':ip}
+    return {'username':user, 'password': password, 'ip_addr':ip}
 
 
 def main():
-	inputs = get_input()
+    indata = get_input()
 	
-	auth = Auth(inputs['username'],inputs['password'])
+    auth = Auth(indata['username'],indata['password'])
     firewall = Firewall()
 
-	if auth.login() != True:
-		raise ValueError("Login failes")
-	else:
-		firewall.accept_ip4(input['ip_addr'])
+    if auth.login() != True:
+        raise ValueError("Login failes")
+    else:
+        firewall.accept_ip4(indata['ip_addr'])
 
 
 if __name__ == '__main__':
---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"></meta>
+<title>DF Firewall Login</title>
+</head>
+<body>
+
+<?php
+
+//Get user ip-address
+$ip_addr = $_SERVER['REMOTE_ADDR'];
+$exec = "python ./df_login.py ";
+
+// 
+if(isset($_POST['user'])){
+    // kjører login scriptet
+
+    $username = $_POST['user'];
+    $password = $_POST['password'];         
+    $cmd = $exec." ".$username." ".$password." ".$ip_addr;
+
+    exec($cmd, $out);
+    print "<h1> Internet enabled </h1> ".$out;
+}
+
+else{ ?>
+
+    <center>
+    <h1>Login to enable internet <h1>
+    <br><b>Logg inn</b><br><br>
+    <form method="POST">
+    Brukernavn:<br>
+    <input type="text" name="user"><br>
+    Passord:<br>
+    <input type="password" name="pass"><br><input value="internetz?" type="submit"></form>
+    </center>
+    <?
+}
+?>
+</body>
+</html>
+
