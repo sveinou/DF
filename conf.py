779aa0eab4cc3b0e65b6c81417c6bb1e6ca313bc
espengjerde <espen.gjerde@gmail.com>
2013-02-27 14:42:39 +0000

    added regexes to conf.py

--- conf.py
+++ conf.py
@@ -16,3 +16,14 @@ class db:
 class dhcp:
     leasefile = "/var/lib/dhcp/dhcpd.leases"
     
+
+## Filters used in the firewall.
+#  Don't mess with this, unless you REALLY KNOW what you are doing.
+#  
+
+class filter:
+    ipv4_in_leasefile = r'([\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3})'
+    mac_in_leasefile = r'([a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9]\:[a-f|0-9][a-f|0-9])'
+    mac_exact = '';
+    ip_exact = r'(^[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}$)'
+
