b73763525231b0ea8c7ef0fd10ca27fd4fb57fec
espengjerde <espen.gjerde@gmail.com>
2013-03-04 16:10:40 +0100

    Oppdattert til å reagere på exit-codes

--- index.php
+++ index.php
@@ -19,9 +19,33 @@ if(isset($_POST['user'])){
     $password = $_POST['pass'];         
     $cmd = $exec." ".$username." ".$password." ".$ip_addr;
 
+    //we should probably check that input before sendingit with sudo... 
+
     print($cmd);
-    exec($cmd, $out);
-    print "<h1> Internet enabled </h1> ".$out;
+    exec($cmd, $out, $code);
+    
+    switch($code){
+        case 0:
+            $msg = "Welcome to the internet."
+        case 1:
+            $msg = "Login failed";
+            break;
+        case 2:
+            $msg = "That combination of MAC and IP seems like horselasagne... ";
+            break;
+        case 2:
+            $msg = "Something went wrong with the input you sent us.";
+        default:
+            $msg = "LOL.\n I DUNNO WHAT HAPPEND.";
+    
+    }
+    ?>
+    <h1><?=$msg?></h1>
+    <p>
+    <em><?=$out[0]?></em><br>
+    EXIT CODE: <?=$code?>
+    </p>
+    <?
 }
 
 else{ ?>
