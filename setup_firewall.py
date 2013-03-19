f37e700ea85743893f5b4cd57fae3e618915e8e4
sveinou <sveinou@sveinou.(none)>
2013-03-19 15:01:17 +0100

    added functions to setup

--- setup_firewall.py
+++ setup_firewall.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+from database.storage import Database
+
 import subprocess as sp
 
 print "Welcome to DF Firewall 0.0001 Alpha"
@@ -13,11 +15,20 @@ my_ip = raw_input()
 print "Enter external interface"
 external_if = raw_input()
 
+print "Use nat? (YES/NO)"
+nat = raw_input()
+
+
+
+
+
 print "\n\nis this your settings?"
 print "External Interface: "+external_if
 print "Internal Interface: "+internal_if
 print "IP internal if: "+my_ip
+print "Use nat? "+nat 
 print "\nEnter YES to continue"
+print " OMG!! DATABASE WILL BE DROPED!!!"
 
 resume = raw_input()
 if resume != "YES":
@@ -38,6 +49,18 @@ fw_rules = ["iptables -F",
 
 dhcp = "service isc-dhcp-server restart"
 
+## Database drop / create tables
+
+sql = "DROP table clients"
+Database().alter(sql)
+sql = "DROP table stats"
+Database().alter(sql)
+sql = "CREATE table clients (User varchar(80), Mac varchar(80), IP4 carchar(80), IP6 varchar(80), active int)"
+Database().alter(sql)
+sql = "CREATE table stats (User varchar(80), Connections bigint(255), tx_total bigint(255), rx_total bigint(255), txs bigint(255), rxs bigint(255), Time timestamp)"
+Database().alter(sql)
+
+
 print "Enabling IPv4 Routing...."
 sp.call(routing, shell=True)
 print "Setting up interface..."
@@ -46,6 +69,12 @@ print "Restarting DHCP-server"
 sp.call(dhcp, shell=True)
 print "Setting up iptables..."
 for rule in fw_rules:
-	sp.call(rule, shell=True)
+	if not "POSTROUTING" in rule:
+		sp.call(rule, shell=True)
+	elif nat = "YES":
+		sp.call(rule, shell=True)
+	else:
+		pass
 
+	
 print "You should be good to go naow."
