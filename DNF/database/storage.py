3d0a6b2fc0650d5eef0b304908006d924774fd4b
espengjerde <espen.gjerde@gmail.com>
2013-03-20 13:47:10 +0100

    NO BØR DET FUNKE§!

---
+++ DNF/database/storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+from datetime import datetime
+import sys, os, time, MySQLdb
+from DNF.conf import db
+
+class Database:
+	con = None
+	cur = None
+
+	def __init__(self):
+		server = db.server
+                user = db.user
+                password = db.pw
+		name = db.name
+		
+                self.con = MySQLdb.connect(server,user,name,password)
+                self.cur = self.con.cursor()
+	
+	def get_row(self,sql):
+	
+		self.cur.execute(sql)
+		return self.cur.fetchone()
+
+	def get_all_rows(self,sql):
+	
+		self.cur.execute(sql)
+		return self.cur.fetchall()
+
+	def alter(self,sql):
+		
+		self.cur.execute(sql)
+		self.con.commit()		
+
+
+	def __del__(self):
+		self.cur.close()
+		self.con.close()
