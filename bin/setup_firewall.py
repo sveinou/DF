980a0ad9c0e6c965ca600eb0665c1dcf0f8de8ca
sveinou <sveino88@gmail.com>
2013-04-04 20:34:23 +0000

    new firewall settings

--- bin/setup_firewall.py
+++ bin/setup_firewall.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from database.storage import Database
+#from DNF.database.storage import Database
 
 import subprocess as sp
 
@@ -39,26 +39,34 @@ interface = "/sbin/ifconfig "+internal_if+" "+my_ip+" netmask 255.255.255.0"
 routing = "echo 1 > /proc/sys/net/ipv4/ip_forward"
 
 fw_rules = ["iptables -F",
-	"iptables -t nat -F",
-	"iptables -A POSTROUTING -t nat -o "+external_if+" -j MASQUERADE",
-	"iptables -t nat -A PREROUTING -p tcp -m multiport --ports 80,443 -j DNAT --to-destination "+my_ip+":80",
-	"iptables -I FORWARD -d "+my_ip+" -p tcp -m multiport --ports 80,443 -j ACCEPT",
-	"iptables -I FORWARD -p udp -m multiport --ports 53 -j ACCEPT",
-	"iptables -A FORWARD -j DROP",
-	"iptables -I INPUT -p tcp --dport 22 -j ACCEPT"]
+        "iptables -t nat -F",
+        "iptables -N ALLOWED",
+        "iptables -N LIMIT",
+        "iptables -N LIMITED",
+        "iptables -t nat -N ALLOWED",
+        "iptables -A FORWARD -p udp -m multiport --ports 53 -j ACCEPT",
+        "iptables -A FORWARD -j LIMITED",
+        "iptables -A FORWARD -j ALLOWED",
+        "iptables -t nat -A PREROUTING -j ALLOWED",
+        "iptables -A POSTROUTING -t nat -o "+external_if+" -j MASQUERADE",
+        "iptables -t nat -A PREROUTING -p tcp -m multiport --ports 80,443 -j DNAT --to-destination "+my_ip+":80",
+        "iptables -A FORWARD -d "+my_ip+" -p tcp -m multiport --ports 80,443 -j ACCEPT",
+        "iptables -A FORWARD -j DROP",
+	"iptables -A LIMIT -m connlimit --connlimit-above 50 -j REJECT",
+        "iptables -I INPUT -p tcp --dport 22 -j ACCEPT"]
 
 dhcp = "service isc-dhcp-server restart"
 
 ## Database drop / create tables
 
-sql = "DROP table clients"
-Database().alter(sql)
-sql = "DROP table stats"
-Database().alter(sql)
-sql = "CREATE table clients (User varchar(80), Mac varchar(80), IP4 carchar(80), IP6 varchar(80), active int)"
-Database().alter(sql)
-sql = "CREATE table stats (User varchar(80), Connections bigint(255), tx_total bigint(255), rx_total bigint(255), txs bigint(255), rxs bigint(255), Time timestamp)"
-Database().alter(sql)
+#sql = "DROP table clients"
+#Database().alter(sql)
+#sql = "DROP table stats"
+#Database().alter(sql)
+#sql = "CREATE table clients (User varchar(80), Mac varchar(80), IP4 carchar(80), IP6 varchar(80), active int)"
+#Database().alter(sql)
+#sql = "CREATE table stats (User varchar(80), Connections bigint(255), tx_total bigint(255), rx_total bigint(255), txs bigint(255), rxs bigint(255), Time timestamp)"
+#Database().alter(sql)
 
 
 print "Enabling IPv4 Routing...."
@@ -71,8 +79,10 @@ print "Setting up iptables..."
 for rule in fw_rules:
 	if not "POSTROUTING" in rule:
 		sp.call(rule, shell=True)
-	elif nat = "YES":
+		print rule
+	elif nat == "YES" or nat == "yes":
 		sp.call(rule, shell=True)
+		print "NAT"
 	else:
 		pass
 
