42d73accc813a95171d8c4a8f75fd398bb67387b
sveinou <sveinou@tihlde.org>
2013-04-11 11:21:50 +0300

    Update df_data.py

--- DNF/database/df_data.py
+++ DNF/database/df_data.py
@@ -13,8 +13,8 @@ class Data:
         sql = sql = "select * from clients where IP4='%s'" % ip4
         return Database().get_row(sql)
         
-    def get_all_users(self):
-    	sql = "select User from clients"
+    def get_all_clients(self):
+    	sql = "select * from clients"
     	return Database().get_all_rows(sql)
 
 
