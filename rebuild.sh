c3dafc483ac7269dfa0b3ca93092c490c4c75d73
espengjerde <espen.gjerde@gmail.com>
2013-04-18 11:15:29 +0200

    rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -3,5 +3,5 @@ pip uninstall DynamicNetworkFirewall
 rm MANIFEST
 rm dist/*
 python setup.py sdist
-pip install dist/DynamicNetworkFirewall-0.3.dev1.tar.gz
+pip install dist/DynamicNetworkFirewall-0.4.alpha.tar.gz
 service apache2 restart
