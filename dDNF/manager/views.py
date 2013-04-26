80f63526ac9a73e4fbaef291d0e677c4c12d6dea
espengjerde <espen.gjerde@gmail.com>
2013-04-26 17:58:37 +0200

    admin

--- dDNF/manager/views.py
+++ dDNF/manager/views.py
@@ -24,13 +24,49 @@ def edit_user(request):
 def blockuser(request):
     pass
 
-def list_active(request):
+def list_active(request, action=None):
     f = Firewall()
+    error = False
+    user = None
+    
+    if action and action == 'limit' and request.POST:
+        user = LoggedInUser.objects.get(last_seen_ip=request.POST.get('victim'))        
+        limit = request.POST.get('setlimit')
+        if user:
+            if limit == 'TX':
+                f.limit_tx(user.last_seen_ip)
+            elif limit == 'RX':
+                f.limit_tx(user.last_seen_ip)
+            elif limit == 'C':
+                f.limit_connections(user.last_seen_ip)
+#            user.is_active = False
+            else:
+                error = True
+        else:
+            error = True
+
+    elif action and action == 'kick' and request.POST:
+        user = LoggedInUser.objects.get(last_seen_ip=request.POST.get('victim'))
+        if user:
+            f.drop_ip4(user.last_seen_ip)
+#            user.is_active = False        
+        else:
+            error = True;
+        
+    elif action and action == 'free' and request.POST:
+        user = LoggedInUser.objects.get(last_seen_ip=request.POST.get('victim'))
+        if user:
+            user.is_active = True
+            f.rm_limit(user.last_seen_ip)
+        else:
+            error = True
+
     allowed = [LoggedInUser.objects.get(last_seen_ip=user[-1]) for user in f.get_allowed() if user[-1] != '0.0.0.0/0']
     limited = [LoggedInUser.objects.get(last_seen_ip=user[-1]) for user in f.get_limited() if user[-1] != '0.0.0.0/0']
-    
-    return render_to_response('active_users.html', {'limited':limited, 'active':allowed}, context_instance=RequestContext(request))
-    
+        
+    return render_to_response('active_users.html', {'limited':limited, 'active':allowed, 'user':user, 'error':error, 'action':action}, context_instance=RequestContext(request))    
+
+
 def firewall(request):
     f = Firewall()
     FORWARD = [{'j': fw[2], 'p':fw[3], 'i':fw[5], 'o':fw[6], 's':fw[7],'d':fw[8], 'rest':fw[9:]} for fw in f.get_custom_forward()]
