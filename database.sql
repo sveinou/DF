c90e8190b7e26975d94d4e7fae070235b2ef6007
sveinou <sveino88@gmail.com>
2013-05-07 19:03:41 +0000

    database changes

--- database.sql
+++ database.sql
@@ -1,15 +1,16 @@
+create database IF NOT EXISTS df;
+grant all privileges on df.* to df@localhost identified by 'df';
+USE df;
 
 
-
-
-CREATE TABLE clients(
+CREATE TABLE IF NOT EXISTS clients(
     user varchar(80), 
     mac varchar(80), 
     ip4 varchar(80), 
     ip6 varchar(80), 
     active int);
     
-CREATE TABLE stats (
+CREATE TABLE IF NOT EXISTS stats (
     user varchar(80), 
     connections bigint(255), 
     tx_total bigint(255), 
@@ -18,7 +19,7 @@ CREATE TABLE stats (
     rxs bigint(255), 
     time timestamp);
 
-CREATE TABLE limited (
+CREATE TABLE IF NOT EXISTS limited (
     User varchar(255),
     CONNLIMIT int(8),
     RXLIMIT int(8),
