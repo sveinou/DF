08ecf9a2ca8169134298a8697c6d26a958d8ecad
Espen Gjærde <espen.gjerde@gmail.com>
2013-05-08 13:34:02 +0300

    Fjerna logger.py

--- DNF/stats/__init__.py
+++ DNF/stats/__init__.py
@@ -2,7 +2,7 @@
 #from df_findip import *
 #from df_user_stats import *
 #from logger import *
-import logger
+#import logger  # DEPRECEATED -- ESPEN, 20130508
 import df_user_stats
 import df_findip
 import bandwidth
