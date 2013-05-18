1938bd24f231446234401da7c9e7220dcb1fadc0
espengjerde <espgjae@gmail.com>
2013-05-18 21:08:49 +0200

    fjerna nettmaske

--- dDNF/statistics/views.py
+++ dDNF/statistics/views.py
@@ -18,7 +18,7 @@ def user_stats(request):
         io = stats.get_iptables_io(ipaddr)
         limited = stats.is_limited(ipaddr)
         limit = [conf.bandwidth.max_connections,conf.bandwidth.max_rxs,conf.bandwidth.max_txs]
-        intip = conf.internal_network
+        intip = conf.internal_network.split('/')[0]
         server = 'http://%s' % (intip)
         return render_to_response('stats.html',{'server':server, 'ip':ipaddr,'io':io,'conn':conn, 'user':user, 'limited': limited, 'limit':limit[0]}, context_instance=RequestContext(request))
 
